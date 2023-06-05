f3df70448b2544ac93441c3fbb062112d5788cbf
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-06-05 17:57:15 +0200

    array add

--- js/script.js
+++ js/script.js
@@ -31,3 +31,6 @@ for (let i = 0; i < listElements.length; i++) {
 
 //PRENDO L'ELEMENTO DEL DOM
 const paragraphElement = document.getElementById('second-paragraph');
+
+//CREO una variabile sfruttando l'array con dentro gli oggetti della lista della spesa
+const listElements2 = ['mela', 'pera', 'pasta', 'pollo', 'cereali', 'mela', 'pera', 'pasta', 'pollo', 'cereali'];
