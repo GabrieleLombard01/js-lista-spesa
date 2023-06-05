c1bdf7a0c679eeba60c6722498ccacbe9b7af7c4
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-06-05 18:13:05 +0200

    while structure

--- js/script.js
+++ js/script.js
@@ -30,7 +30,16 @@ for (let i = 0; i < listElements.length; i++) {
 */
 
 //PRENDO L'ELEMENTO DEL DOM
-const paragraphElement = document.getElementById('second-paragraph');
+const paragraphElement2 = document.getElementById('second-paragraph');
 
 //CREO una variabile sfruttando l'array con dentro gli oggetti della lista della spesa
 const listElements2 = ['mela', 'pera', 'pasta', 'pollo', 'cereali', 'mela', 'pera', 'pasta', 'pollo', 'cereali'];
+
+let list2 = ' ';
+
+//APRO UN CICLO
+while () {
+    list2 += `<li>${listElements2[i]}</li>`;
+
+    paragraphElement2.innerHTML = list2;
+};
